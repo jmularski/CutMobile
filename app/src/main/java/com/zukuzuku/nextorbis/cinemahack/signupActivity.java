8cf951a777c6676825bde2348bb6a769b29c0dba
Norman Lista <normanusb@gmail.com>
2018-08-09 23:46:00 +0200

    Finish login  and signup activities

--- app/src/main/java/com/zukuzuku/nextorbis/cinemahack/signupActivity.java
+++ app/src/main/java/com/zukuzuku/nextorbis/cinemahack/signupActivity.java
@@ -1,29 +1,42 @@
 package com.zukuzuku.nextorbis.cinemahack;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class signupActivity extends AppCompatActivity {
-
+    private Button signup;
+    EditText name, lastName, email, password, adress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        signup= (Button) findViewById(R.id.button_singupdone);
+        name= (EditText) findViewById(R.id.editText_namesign);
+        lastName= (EditText) findViewById(R.id.editText_lastnamesign);
+        email =(EditText) findViewById(R.id.editText_emailsign);
+        password= (EditText) findViewById(R.id.editText_signpass);
+        adress= (EditText) findViewById(R.id.editText_signadress);
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                gosingin();
             }
         });
+
     }
 
+    public void  gosingin () {
+
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+
+    }
 }
+
