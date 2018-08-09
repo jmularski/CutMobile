d45a1dc0aa6700effba00a26a25b2db7428fa25c
Jakub Mularski <dragt12@o2.pl>
2018-08-10 00:12:39 +0200

    Added facebook login

--- app/src/main/java/com/zukuzuku/nextorbis/cinemahack/LoginActivity.java
+++ app/src/main/java/com/zukuzuku/nextorbis/cinemahack/LoginActivity.java
@@ -3,21 +3,30 @@ package com.zukuzuku.nextorbis.cinemahack;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.cloudrail.si.CloudRail;
+import com.cloudrail.si.interfaces.Profile;
+import com.cloudrail.si.services.Facebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
-    Button login, signin;
+    Button login, signin, facebook;
     EditText username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        CloudRail.setAppKey("5b6cb81b70a77f7d86b61746");
         login= (Button) findViewById(R.id.buttonlogin);
         signin=(Button) findViewById(R.id.button_signin);
+        facebook = (Button) findViewById(R.id.button_facebook);
         username =(EditText) findViewById(R.id.editText_user);
         password= (EditText) findViewById(R.id.edittextPassword);
 
@@ -28,7 +37,26 @@ public class LoginActivity extends AppCompatActivity {
                 //login();
             }
         });
-
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<String> scope = new ArrayList<>();
+                scope.add("public_profile");
+                scope.add("email");
+                final Profile profile = new Facebook(LoginActivity.this, "516044692140717", "23d761b45629854248870714560874d5", scope);
+                new Thread() {
+                    @Override
+                    public void run() {
+                        String fullName = profile.getFullName();
+                        String email = profile.getEmail();
+                        Log.d("fullname", fullName);
+                        Log.d("email", email);
+                        Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                        startActivity(intent);
+                    }
+                }.start();
+            }
+        });
         signin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
